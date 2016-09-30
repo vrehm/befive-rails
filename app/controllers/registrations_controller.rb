a2efd571f529550dba8327ae6b71d9da38e392c7
Hedi Jabri <hedi.jbi@gmail.com>
2016-09-30 11:19:45 +0200

    Add user infos in views

--- app/controllers/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-  protected
-  
-  def after_update_path_for(current_user)
-    pages_dashboard_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users
 
   root to: 'pages#home'
-  get 'pages/dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
