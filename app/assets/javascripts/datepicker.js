1ae5ca77e6ce7bdc98b9261ce01223122681b182
Hedi Jabri <hedi.jbi@gmail.com>
2016-10-08 10:40:36 +0200

    Remove default class active to list team cards

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -3,7 +3,7 @@
 		days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
 		daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
 		daysMin: ["d", "l", "ma", "me", "j", "v", "s"],
-		months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+		months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
 		monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
 		today: "Aujourd'hui",
 		monthsTitle: "Mois",
