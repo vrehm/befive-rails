0ea9601961dc3b2f0f12a9a6cfc405799e80f968
Hedi Jabri <hedi.jbi@gmail.com>
2016-10-20 21:13:43 +0200

    Change wording in welcome mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize(@user)
     @message = Message.new(user: current_user, recipient_email: @user.email, recipient_first_name: @user.first_name)
-    @team = @user.members.first.team
+    if current_user.members.any?
+      @team = @user.members.first.team
+    end
   end
 end
